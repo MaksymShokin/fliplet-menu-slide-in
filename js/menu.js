c60d01d9df3523bb568331adfcd3ef0feabfa60b
Hugo Carneiro <hcarneiro@weboo.biz>
2017-05-08 11:51:05 +0100

    Added backdrop filter

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,8 @@
-$('.fl-menu-overlay').click(function () {
+if (Modernizr.backdropfilter) {
+  $('.body').addClass('backdropfilter');
+}
+
+$('.fl-menu-overlay').click(function() {
   $(this).closest('.fl-menu').removeClass('active');
 });
 
